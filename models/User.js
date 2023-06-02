051c026611aeb3786c69058428d568a55a9460af
Emma Carr <lorenagracex@gmail.com>
2023-06-01 19:19:56 -0500

    user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model, Schema, Schema} = require('mongoose');
+
+// define user data
+const Schema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /.+\@.+\..+/, // Regex for @  of email
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+user.Schema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
